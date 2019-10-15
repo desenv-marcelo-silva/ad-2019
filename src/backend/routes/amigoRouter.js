2c4cca8fe73ed3c81c27c2c02e8f1e9e14b3e4bc
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2019-10-15 19:23:42 -0300

    Remoção de pasta desnecessária.

--- src/backend/routes/amigoRouter.js
+++ src/backend/routes/amigoRouter.js
@@ -1,52 +1,55 @@
-const express = require('express');
-const amigoModel = require('../models/Amigo.js');
+const express = require("express");
+const amigoModel = require("../models/Amigo.js");
 const app = express();
-const shuffleArray = require('./shuffleArray.js');
-const enviarEmail = require('../config/email.js');
+const shuffleArray = require("./shuffleArray.js");
+const enviarEmail = require("../config/email.js");
 
-app.get('/sorteio', async function(req, res){
+app.get("/sorteio", async function(req, res) {
   const listaAmigos = await amigoModel.find({});
 
   var listaAmigosSorteio = [...listaAmigos];
-  
+
   shuffleArray(listaAmigosSorteio);
-  
+
   var tamanhoMaximo = listaAmigos.length;
   var listaSorteada = [];
-  
-  for(var x = 0; x<tamanhoMaximo-1;x++) {
-    listaSorteada
-    .push({id: listaAmigosSorteio[x].id, 
-      nome: listaAmigosSorteio[x].nome, 
-      amigo: listaAmigosSorteio[x+1].nome, email: listaAmigosSorteio[x+1].email});
+
+  for (var x = 0; x < tamanhoMaximo - 1; x++) {
+    listaSorteada.push({
+      id: listaAmigosSorteio[x].id,
+      nome: listaAmigosSorteio[x].nome,
+      amigo: listaAmigosSorteio[x + 1].nome,
+      email: listaAmigosSorteio[x + 1].email
+    });
   }
-  listaSorteada
-  .push({id: listaAmigosSorteio[x].id, 
-    nome: listaAmigosSorteio[x].nome, 
+  listaSorteada.push({
+    id: listaAmigosSorteio[x].id,
+    nome: listaAmigosSorteio[x].nome,
     amigo: listaAmigosSorteio[0].nome,
-  email: listaAmigosSorteio[0].email});
-    
-  listaSorteada.forEach(function(item){
+    email: listaAmigosSorteio[0].email
+  });
+
+  listaSorteada.forEach(function(item) {
     try {
-        amigoModel.findByIdAndUpdate(item.id, {nomeAmigo: item.amigo})      
+      amigoModel.findByIdAndUpdate(item.id, { nomeAmigo: item.amigo });
     } catch (err) {
       console.log(err);
-    }    
+    }
     const data = {
-      from: "Mailgun Sandbox <postmaster@sandbox838c04dbb0a648af8f3fa09feed3c161.mailgun.org>",
+      from:
+        "Mailgun Sandbox <postmaster@sandbox838c04dbb0a648af8f3fa09feed3c161.mailgun.org>",
       to: item.email,
       subject: "Resultado do amigo secreto",
       text: `Parabéns por participar do nosso amigo secreto!<br />A pessoa que você tirou foi ${item.amigo}`
     };
-    
-    enviarEmail.mensagens.messages().send(data, 
-      function (error, body) {
-        console.log(body);
+
+    enviarEmail.mensagens.messages().send(data, function(error, body) {
+      console.log(body);
     });
-  })
+  });
 });
 
-app.get('/amigos', async (req, res) => {
+app.get("/amigos", async (req, res) => {
   const amigos = await amigoModel.find({});
 
   try {
@@ -56,7 +59,7 @@ app.get('/amigos', async (req, res) => {
   }
 });
 
-app.post('/amigo', async (req, res) => {
+app.post("/amigo", async (req, res) => {
   const amigo = new amigoModel(req.body);
 
   try {
@@ -67,25 +70,25 @@ app.post('/amigo', async (req, res) => {
   }
 });
 
-app.delete('/amigo/:id', async (req, res) => {
+app.delete("/amigo/:id", async (req, res) => {
   try {
-    const amigo = await amigoModel.findByIdAndDelete(req.params.id)
+    const amigo = await amigoModel.findByIdAndDelete(req.params.id);
 
-    if (!amigo) res.status(404).send("No item found")
-    res.status(200).send()
+    if (!amigo) res.status(404).send("Item não encontrado.");
+    res.status(200).send();
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
-app.patch('/amigo/:id', async (req, res) => {
+app.patch("/amigo/:id", async (req, res) => {
   try {
-    await amigoModel.findByIdAndUpdate(req.params.id, req.body)
-    res.status(200).send()
+    await amigoModel.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send();
   } catch (err) {
     console.log(err);
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
-module.exports = app
+module.exports = app;
--- testarConexao.js
+++ testarConexao.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
-const keys = require('./src/core/keys.js');
+const mongoose = require("mongoose");
+const keys = require("./src/core/keys.js");
 
 //mongoose.connect('mongodb+srv://userName:Passwrod@cluster.mongodb.net/', {dbName: 'yourDbName'});
 //mongodb://testeadireto:<password>@cluster0-shard-00-00-gswb9.mongodb.net:27017,cluster0-shard-00-01-gswb9.mongodb.net:27017,cluster0-shard-00-02-gswb9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
-mongoose.connect("mongodb://testeadireto:rlJF3cPyODpBbWNW@cluster0-shard-00-00-gswb9.mongodb.net:27017,cluster0-shard-00-01-gswb9.mongodb.net:27017,cluster0-shard-00-02-gswb9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true  
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+mongoose
+  .connect(
+    "mongodb://xxx:yyy@cluster0-shard-00-00-gswb9.mongodb.net:27017,cluster0-shard-00-01-gswb9.mongodb.net:27017,cluster0-shard-00-02-gswb9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
-
+  });
